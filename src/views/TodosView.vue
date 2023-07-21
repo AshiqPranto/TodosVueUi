74125d8d726824c3d7258eebbe83c21557019512
AshiqPranto <ashiqpranto@gmail.com>
2023-07-21 18:54:35 +0600

    completed till emmit with ref

---
+++ src/views/TodosView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import ToDoCreator from '../components/ToDoCreator.vue';
+import {uid} from 'uid'
+import { ref } from 'vue';
+
+const todoList = ref([]);
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: false,
+    isEditing: null,
+  });
+};
+</script>
+
+<template>
+  <main>
+    <h1>Create Todo</h1>
+
+    <ToDoCreator @create-todo="createTodo"/>
+
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
